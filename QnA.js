34c913d08b583964b65edff9c9019e205c268e82
Himanshuyadav560 <105383081+Himanshuyadav560@users.noreply.github.com>
2023-08-10 11:45:23 +0530

    Add files via upload

---
+++ QnA.js
@@ -0,0 +1,8 @@
+// // JavaScript code for adding interactivity
+// const questions = document.querySelectorAll('.question');
+
+// questions.forEach(question => {
+//   question.addEventListener('click', () => {
+//     question.classList.toggle('active');
+//   });
+// });
